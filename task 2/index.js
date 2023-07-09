baf5c794a3830ca1b0fb64451cf756af70c13e71
anyefremova <anna.efremova.02@yandex.ru>
2023-07-09 17:59:37 +0500

    доделала 2 задание

--- task 2/index.js
+++ task 2/index.js
@@ -1,14 +1,46 @@
 function calcDaysUntilBirthday() {
-    const date = document.querySelector("#date").value;
-    const currentDate = new Date();
-    const textResult = document.querySelector("#result")
+    const date = document.querySelector("#date");
+    const dateStr = date.value;
+    const dateTimestamp = Date.parse(dateStr);
+    const currentTimesramp = new Date();
+    const textResult = document.querySelector("#result");
+    const error = document.querySelector("#error");
 
-    const dif = date - currentDate;
-    const result = Math.floor(dif / (1000 * 60 * 60 * 24));
-    console.log(date);
-    console.log(result);
-    textResult.innerText = 'Осталось ' + result + ' дней';
+    function getDates(number, one, two, three) {
+        let num = Math.abs(number);
+
+        num %= 100;
+        if (num >= 5 && num <= 20) {
+            return three;
+        }
+
+        num %= 10;
+        if (num === 1) {
+            return one;
+
+        } else if (num >= 2 && num <= 4) {
+            return two;
+        } else {
+            return three;
+        }
+    }
+    console.log(getDates(1, "день", "дня", "дней"));
+
+    const dateDif = dateTimestamp - currentTimesramp;
+    const result = Math.floor(dateDif / (1000 * 60 * 60 * 24));
+
+    if (dateStr === '') {
+        error.innerText = 'Пожалуйста, введите дату рождения';
+    } else {
+        error.innerText = '';
+    }
+
+    if (dateStr === '') {
+        textResult.innerText = '';
+    } else {
+        textResult.innerText = 'Осталось ' + result + ' ' + getDates(result, "день", "дня", "дней");
+    }
 }
 
 const button = document.querySelector("#calc");
-button.addEventListener('click', calcDaysUntilBirthday)
+button.addEventListener('click', calcDaysUntilBirthday);
